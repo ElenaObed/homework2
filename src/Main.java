9af2da7094da29cf2b382b6e92263a9c8cbade58
Elena Obed <elena_sergeevna137@mail.ru>
2023-05-03 23:16:55 +0300

    modification3

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Здравствуйте");
-        int tiket = 30_000;
-        System.out.println("Стоимость билета: " + tiket + " руб.");
+        int ticket = 30_000;
+        System.out.println("Стоимость билета: " + ticket + " руб.");
         int oneMiles = 20;
-        int bonusMiles = tiket / oneMiles;
+        int bonusMiles = ticket / oneMiles;
         System.out.println("Ваш бонус составляет: " + bonusMiles);
     }
 }
