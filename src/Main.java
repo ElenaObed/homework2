4bed47f67c1629817117eff799f234267f0900a5
Elena Obed <elena_sergeevna137@mail.ru>
2023-05-03 16:27:21 +0300

    calculation2

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Здравствуйте");
 
-        int tiket = 13_676;
+        int tiket = 30_000;
         System.out.println("Стоимость билета: " + tiket + "руб.");
         int onemiles = 20;
-
+пшп
         int bonusmiles = tiket / onemiles;
 
         System.out.println("Ваш бонус составляет: " + bonusmiles);
